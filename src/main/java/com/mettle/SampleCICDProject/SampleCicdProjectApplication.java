6f6a4d02e84cf0f784ac37c7f6aeb320c7a4eeb7
Raihan Ahmed <raihan.ahmed@mettles.com>
2021-04-23 16:07:39 -0400

    Sample spring boot project with spring web enabled

---
+++ src/main/java/com/mettle/SampleCICDProject/SampleCicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mettle.SampleCICDProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCicdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCicdProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mettle/SampleCICDProject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mettle.SampleCICDProject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleCicdProjectApplication.class);
+	}
+
+}
